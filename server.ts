97a2dacf66253119ac9eae872b870d8150bc7858
Xx159753@@ <xxyyzz2050@gmail.com>
2019-06-12 13:02:50 +0300

    config firebase for ssr

--- server.ts
+++ server.ts
@@ -8,6 +8,10 @@ import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 import * as express from 'express';
 import { join } from 'path';
 
+// for firebase
+import { readFileSync } from 'fs';
+import { renderModuleFactory } from '@angular/platform-server';
+
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
 
@@ -23,14 +27,29 @@ const {
   LAZY_MODULE_MAP
 } = require('./dist/server/main');
 
-// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+// firebase:
+/*// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine(
   'html',
-  ngExpressEngine({
+  ngExpressEngine({ //returns renderModuleFactory(path,options,callback)
     bootstrap: AppServerModuleNgFactory,
     providers: [provideModuleMap(LAZY_MODULE_MAP)]
   })
-);
+);*/
+(global as any).WebSocket = require('ws');
+(global as any).XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+app.engine('html', (filePath, options, callback) => {
+  // todo: use ngExpressEngine() as a second parameter
+  renderModuleFactory(AppServerModuleNgFactory, {
+    document: readFileSync(join(DIST_FOLDER, 'index.html')).toString(),
+    url: options.req.url,
+    // DI so that we can get lazy-loading to work differently (since we need it to just instantly render it)
+    extraProviders: [provideModuleMap(LAZY_MODULE_MAP)]
+  }).then(html => {
+    callback(null, html);
+  });
+});
 
 app.set('view engine', 'html');
 app.set('views', DIST_FOLDER);
