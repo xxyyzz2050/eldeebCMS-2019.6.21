731e1bc2ad1ec28725a55d9332842ba670c4e68d
admin <admin@almogtama3.com>
2019-06-12 23:29:27 +0300

    eldeeb: apply tslint

--- projects/eldeeb/src/lib/general.ts
+++ projects/eldeeb/src/lib/general.ts
@@ -6,11 +6,11 @@ todo:
 - run(): mark may be object: arguments -> if(mark:obj && mark.collee)mark={run:mark/mark.callee.name,...arguments}
 */
 
-import util from "util";
+import util from 'util';
 
 export function exportAll(module: any) {
   Object.keys(module).forEach(key => {
-    exports[key] = module[key]; //todo: ES export i.e export key = mongoose[key]
+    exports[key] = module[key]; // todo: ES export i.e export key = mongoose[key]
   });
 }
 
@@ -21,13 +21,13 @@ export function exportAll(module: any) {
  * @param  type="error" any console method, such as log, error, warn
  */
 
-export function log(obj: any, type = "error"): void {
+export function log(obj: any, type = 'error'): void {
   obj = util.inspect(obj, {
     maxArrayLength: null,
     depth: null,
     colors: true,
     breakLength: 100
-    //compact: false,
+    // compact: false,
   });
 
   console[type](obj);
@@ -46,8 +46,8 @@ export function isIterable(obj: any): boolean {
   return (
     obj &&
     (obj instanceof Array ||
-      (typeof obj != "string" && typeof obj[Symbol.iterator] == "function"))
-    //todo: or obj={...}
+      (typeof obj != 'string' && typeof obj[Symbol.iterator] == 'function'))
+    // todo: or obj={...}
   );
 }
 
@@ -62,21 +62,22 @@ export function isIterable(obj: any): boolean {
 export function inArray(
   element: any,
   array: Array<any> | object | string,
-  caseSensitive?: boolean //case sensitive
+  caseSensitive?: boolean // case sensitive
 ): boolean {
-  if (!caseSensitive && typeof element == "string")
+  if (!caseSensitive && typeof element == 'string') {
     element = element.toLowerCase();
-  if (typeof array == "string") return !!array.indexOf(element); //!! to convert umber to boolean
+  }
+  if (typeof array == 'string') { return !!array.indexOf(element); } // !! to convert umber to boolean
   if (isIterable(array)) {
-    for (let i = 0; i < (<Array<any>>array).length; i++) {
+    for (let i = 0; i < (array as Array<any>).length; i++) {
       return (
         array[i] == element ||
         (!caseSensitive &&
-          typeof array[i] == "string" &&
+          typeof array[i] == 'string' &&
           array[i].toLowerCase() == element)
       );
     }
-  } else if (typeof array == "object") return element in array;
+  } else if (typeof array == 'object') { return element in array; }
 }
 
 /**
@@ -87,7 +88,7 @@ export function inArray(
  * @return [description]
  */
 export function sleep(seconds?: number): Promise<void> {
-  //ex: this.run(async fn(){await this.sleep(1); alert(1);})
+  // ex: this.run(async fn(){await this.sleep(1); alert(1);})
   return new Promise(resolve => setTimeout(resolve, (seconds || 2) * 1000));
 }
 
@@ -108,8 +109,8 @@ export function sleep(seconds?: number): Promise<void> {
 export function objectType(obj: any): string {
   return Object.prototype.toString
     .call(obj)
-    .replace("[object ", "")
-    .replace("]", "")
+    .replace('[object ', '')
+    .replace(']', '')
     .toLowerCase();
   /*
     examples:
@@ -123,29 +124,29 @@ export function objectType(obj: any): string {
 }
 
 export function isEmpty(obj: any): boolean {
-  return typeof obj == "undefined" || inArray(obj, ["", null, [], {}]);
+  return typeof obj == 'undefined' || inArray(obj, ['', null, [], {}]);
 }
 
 export function merge(target: any, ...obj: any[]): any {
-  //merge objects,arrays,classes (must besame type) ;
-  //don't use "arguments" in an arrow functions, also don't use 'this' inside a normal function, so we declare a new variable = arguments
-  let _arg = arguments; //the arguments of merge() not run()
-  return this.run({ run: "merge", ...arguments }, () => {
-    let type = objectType(target); //todo: error: Cannot read property 'objectType' of undefined
+  // merge objects,arrays,classes (must besame type) ;
+  // don't use "arguments" in an arrow functions, also don't use 'this' inside a normal function, so we declare a new variable = arguments
+  const _arg = arguments; // the arguments of merge() not run()
+  return this.run({ run: 'merge', ...arguments }, () => {
+    const type = objectType(target); // todo: error: Cannot read property 'objectType' of undefined
     for (let i = 1; i < _arg.length; i++) {
-      if (this.objectType(_arg[i]) !== type) return target;
+      if (this.objectType(_arg[i]) !== type) { return target; }
     }
-    if (type == "array") {
+    if (type == 'array') {
       target = target.concat(...obj);
-    } else if (type == "object") {
-      //target=Object.assign(target,...obj) //later objects dosen't override previous ones
+    } else if (type == 'object') {
+      // target=Object.assign(target,...obj) //later objects dosen't override previous ones
       for (let i = 1; i < _arg.length; i++) {
-        for (let p in _arg[i]) {
-          target[p] = _arg[i][p]; //to override current values
+        for (const p in _arg[i]) {
+          target[p] = _arg[i][p]; // to override current values
         }
       }
-    } else if (type == "class") {
-      //add or override target's methods & properties
+    } else if (type == 'class') {
+      // add or override target's methods & properties
     }
 
     return target;
@@ -153,18 +154,19 @@ export function merge(target: any, ...obj: any[]): any {
 }
 
 export function json(data: string | object) {
-  if (typeof data == "string") {
-    if (data.trim().charAt(0) == "{") return JSON.parse(data);
+  if (typeof data == 'string') {
+    if (data.trim().charAt(0) == '{') { return JSON.parse(data); }
     if (
       data
-        .split(".")
+        .split('.')
         .pop()
-        .toLowerCase() == "json"
-    )
-      return require(data); //load a .json file, todo: ES2015 modules don't allow dynamic imports, and causes typescript error: 'Critical dependency: the request of a dependency is an expression'
-    //todo: move this part to files class
-    data = require("fs").readFileSync(data);
-    return JSON.parse(<string>data);
-  } else return JSON.stringify(data);
-  //nx: if(string & !start)
+        .toLowerCase() == 'json'
+    ) {
+      return require(data);
+    } // load a .json file, todo: ES2015 modules don't allow dynamic imports, and causes typescript error: 'Critical dependency: the request of a dependency is an expression'
+    // todo: move this part to files class
+    data = require('fs').readFileSync(data);
+    return JSON.parse(data as string);
+  } else { return JSON.stringify(data); }
+  // nx: if(string & !start)
 }
