64c21a4df20bdf0e0de3dc5c7c89d00b7dc6559b
Xx159753@@ <xxyyzz2050@gmail.com>
2019-06-17 14:35:03 +0300

    fix: firebase deploy
    - firebase build output = /dist
     i.e with Angular output dir
      -> no need to copy ./dist to functions/dist
    
    - firebase modules are installed in ./package.json instead of 
    functions/package.json
    
    - firebase.json/predeploy removed as firebase already build via `npm run 
    build:firebase` before deploying
     check package.json/scripts/deploy:firebase
    
    todo: put all firebase files inside ./firebase/
     i.e: functions/, firebase.json, ...

--- functions/copy-angular.ts
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra');
-
-(async () => {
-  const src = '../dist';
-  const copy = './dist';
-
-  await fs.remove(copy);
-  await fs.copy(src, copy);
-})();
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,39 @@
-import * as functions from 'firebase-functions';
-
 /* Start writing Firebase Functions
 https://firebase.google.com/docs/functions/typescript
+*/
 
+/*
 export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+  response.send('Hello from Firebase!');
+});
+*/
+
+require('zone.js/dist/zone-node');
+
+const functions = require('firebase-functions');
+const express = require('express');
+const path = require('path');
+const { enableProdMode } = require('@angular/core');
+const { renderModuleFactory } = require('@angular/platform-server');
+const { AppServerModuleNgFactory } = require('../server/main');
+
+enableProdMode();
+
+const index = require('fs')
+  .readFileSync(path.resolve(__dirname, './index.html'), 'utf8')
+  .toString();
+
+const app = express();
+
+app.get('**', (req, res) => {
+  renderModuleFactory(AppServerModuleNgFactory, {
+    url: req.path,
+    document: index
+  }).then(html => res.status(200).send(html));
 });
 
-*/
+exports.ssr = functions.https.onRequest(app);
+/*
+const universal = require(`${process.cwd()}/dist/server`).app;
+export const ssr = functions.https.onRequest(universal);
+ */
